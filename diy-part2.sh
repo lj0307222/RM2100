94f36d9b2df50220671c065d48ff52efcc6594ca
lj0307222 <61595433+lj0307222@users.noreply.github.com>
2020-08-06 16:06:51 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,16 +12,6 @@
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 
-
-git clone https://github.com/jerrykuku/luci-app-vssr.git package/lean/luci-app-vssr
-git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb
-echo 'CONFIG_PACKAGE_luci-app-vssr=y' >> .config
-echo 'CONFIG_PACKAGE_luci-app-vssr_INCLUDE_V2ray=y' >> .config
-echo 'CONFIG_PACKAGE_luci-app-vssr_INCLUDE_Trojan=y' >> .config
-echo 'CONFIG_PACKAGE_luci-app-vssr_INCLUDE_ShadowsocksR_Server=y' >> .config
-echo 'CONFIG_PACKAGE_luci-app-vssr_INCLUDE_ShadowsocksR_Socks=y' >> .config
-echo 'CONFIG_PACKAGE_luci-i18n-vssr-zh-cn=y'  >> .config
-
 git clone https://github.com/frainzy1477/luci-app-clash package/lean/luci-app-clash
 echo 'CONFIG_PACKAGE_luci-app-clash=y' >> .config
 
